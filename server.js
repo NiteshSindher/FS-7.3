03c1c619a1c3cdd2c82749dd3b2794ba6ee25852
NiteshSindher <sindhernitesh2005@gmail.com>
2025-10-30 14:03:02 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000", // React frontend
+    methods: ["GET", "POST"],
+  },
+});
+
+// Listen for socket connections
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("send_message", (data) => {
+    io.emit("receive_message", data); // Broadcast to all users
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
